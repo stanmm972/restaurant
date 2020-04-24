a1f012c478de509ab7b0edca8b78ed463fc73874
fred maurice <fmmadelon@gmail.com>
2020-04-24 14:16:15 -0400

    added cart, order controller, model controller, cart view

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -3,6 +3,7 @@ class MenuController < ApplicationController
   def index
     @page = 'menu'
     @products = Product.all
+    @order_item = current_order.order_items.new
   end
 
   def search
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :order_items
+  get 'cart', to: 'cart#show'
   get 'search', to: 'menu#search'
   resources :categories
   get 'menu', to: 'menu#index'
